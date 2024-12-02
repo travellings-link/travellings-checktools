8f9ccb9d731aa380162d38f18b4f424cf5a8997e
Big_Cake <me@lihaoyu.cn>
2024-12-02 21:51:11 +0800

    fix: API wrap

--- src/utils/tApi.ts
+++ src/utils/tApi.ts
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
-const headers: any = {
-  'Content-Type': 'application/json',
-};
-
-if (typeof window !== 'undefined') {
-  headers['Cookie'] = `_tLogin=${localStorage.getItem('_tLogin')}`;
-}
-
 const api = axios.create({
   baseURL: 'https://api.travellings.cn',
   timeout: 5000,
-  headers: headers,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
